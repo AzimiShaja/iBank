914fc1551ab3a1ea1f3fc0632fb6648b17126c4e
Ahmad Shaja AZIMI <ahmad.shojaazimi@gmail.com>
2023-08-24 22:48:50 +0300

    fixed nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,9 @@ export default function Header() {
   }
   return (
     <>
-      <div className="bg-black py-3 text-white flex justify-between lg:px-20 max-lg:hidden">
+      <div
+        className={`bg-black py-3 text-white flex justify-between lg:px-20 max-lg:hidden`}
+      >
         <div className="flex gap-4">
           <h1 className="flex items-center gap-1">
             <HiMail className="text-orange-500" /> support@ibank.com
@@ -47,22 +49,77 @@ export default function Header() {
         </div>
       </div>
       {/* =========Header========= */}
-
-      <header
-        className={`flex ${
-          sticky ? "sticky bg-white duration-500 top-0 z-50" : ""
-        } items-center justify-between py-4 px-5 lg:px-20 shadow-md border-b-4 border-orange-500 `}
+      <div
+        className={`${
+          sticky ? "sticky top-0 z-50 bg-white duration-500 " : " "
+        }`}
       >
-        <Link to={"/"} className="cursor-pointer">
-          <h1 className="text-3xl font-bold font-pacifico">
-            <span className="text-orange-500 font-pacifico mr-1 text-4xl">
-              i
-            </span>
-            Bank
-          </h1>
-        </Link>
-        <div className="flex gap-6 items-center max-lg:hidden">
-          <ul className="flex gap-4 text-lg nav-links">
+        <header
+          className={`flex items-center justify-between py-4 px-5 lg:px-20 shadow-md border-b-4 border-orange-500 `}
+        >
+          <Link to="/" className="cursor-pointer">
+            <h1 className="text-3xl font-bold font-pacifico">
+              <span className="text-orange-500 font-pacifico mr-1 text-4xl">
+                i
+              </span>
+              Bank
+            </h1>
+          </Link>
+          <div className="flex gap-6 items-center max-lg:hidden">
+            <ul className="flex gap-4 text-lg nav-links">
+              <li
+                className={` ${activeNav === 0 ? "active-nav" : ""}`}
+                onClick={() => setActiveNav(0)}
+              >
+                Home
+              </li>
+              <li
+                className={`${activeNav === 1 ? "active-nav" : ""} `}
+                onClick={() => setActiveNav(1)}
+              >
+                About us
+              </li>
+              <li
+                className={`${activeNav === 2 ? "active-nav" : ""}`}
+                onClick={() => setActiveNav(2)}
+              >
+                Service
+              </li>
+              <li
+                className={`${activeNav === 3 ? "active-nav" : ""}`}
+                onClick={() => setActiveNav(3)}
+              >
+                Contact
+              </li>
+              <li>login</li>
+            </ul>
+            <Link to={"/register"}>
+              <button className="bg-blue-900 py-3 px-7 border border-blue-900 text-white rounded-lg hover:bg-white hover:text-black duration-300">
+                Register
+              </button>
+            </Link>
+          </div>
+          {/* =========Hamburger Menu========= */}
+          {isOpen ? (
+            <GiTireIronCross
+              onClick={handleToggle}
+              className="text-orange-500 text-4xl lg:hidden"
+            />
+          ) : (
+            <button className="lg:hidden">
+              <RxHamburgerMenu
+                onClick={handleToggle}
+                className="text-orange-500 text-4xl"
+              />
+            </button>
+          )}
+        </header>
+        <div
+          className={`flex flex-col gap-6 items-center shadow-xl py-10 px-10 lg:hidden ${
+            isOpen ? "show_links" : "hide_links"
+          }`}
+        >
+          <ul className="flex flex-col items-center gap-4 text-lg nav-links">
             <li
               className={` ${activeNav === 0 ? "active-nav" : ""}`}
               onClick={() => setActiveNav(0)}
@@ -89,62 +146,10 @@ export default function Header() {
             </li>
             <li>login</li>
           </ul>
-          <Link to={"/register"}>
-            <button className="bg-blue-900 py-3 px-7 border border-blue-900 text-white rounded-lg hover:bg-white hover:text-black duration-300">
-              Register
-            </button>
-          </Link>
-        </div>
-        {/* =========Hamburger Menu========= */}
-        {isOpen ? (
-          <GiTireIronCross
-            onClick={handleToggle}
-            className="text-orange-500 text-4xl lg:hidden"
-          />
-        ) : (
-          <button className="lg:hidden">
-            <RxHamburgerMenu
-              onClick={handleToggle}
-              className="text-orange-500 text-4xl"
-            />
+          <button className="bg-blue-900 py-3 px-7 text-white rounded-lg sm:w-6/12 w-full">
+            Register
           </button>
-        )}
-      </header>
-      <div
-        className={`flex flex-col gap-6 items-center shadow-xl py-10 px-10 lg:hidden ${
-          isOpen ? "show_links" : "hide_links"
-        }`}
-      >
-        <ul className="flex flex-col items-center gap-4 text-lg nav-links">
-          <li
-            className={` ${activeNav === 0 ? "active-nav" : ""}`}
-            onClick={() => setActiveNav(0)}
-          >
-            Home
-          </li>
-          <li
-            className={`${activeNav === 1 ? "active-nav" : ""} `}
-            onClick={() => setActiveNav(1)}
-          >
-            About us
-          </li>
-          <li
-            className={`${activeNav === 2 ? "active-nav" : ""}`}
-            onClick={() => setActiveNav(2)}
-          >
-            Service
-          </li>
-          <li
-            className={`${activeNav === 3 ? "active-nav" : ""}`}
-            onClick={() => setActiveNav(3)}
-          >
-            Contact
-          </li>
-          <li>login</li>
-        </ul>
-        <button className="bg-blue-900 py-3 px-7 text-white rounded-lg sm:w-6/12 w-full">
-          Register
-        </button>
+        </div>
       </div>
     </>
   );
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -5,13 +5,13 @@ export default function RegisterForm() {
         <h1 className="text-4xl font-bold font-open">Register</h1>
         <form className="register flex flex-col gap-4 w-full">
           <div className="flex gap-4 md:gap-5 justify-center max-md:flex-col">
-            <input type="text" placeholder="Enter your first name" />
-            <input type="text" placeholder="Enter your Last name" />
+            <input type="text" placeholder="Enter your first name" required />
+            <input type="text" placeholder="Enter your Last name" required />
           </div>
-          <input type="date" placeholder="date of birth" />
-          <input type="email" placeholder="Enter your email" />
-          <input type="password" placeholder="Enter your password" />
-          <input type="password" placeholder="confirm your password" />
+          <input type="date" placeholder="date of birth" required />
+          <input type="email" placeholder="Enter your email" required />
+          <input type="password" placeholder="Enter your password" required />
+          <input type="password" placeholder="confirm your password" required />
           <button className="bg-blue-900 py-2 border border-blue-900 text-white rounded-lg hover:bg-white hover:text-black duration-300 cursor-pointer">
             Sign up
           </button>
